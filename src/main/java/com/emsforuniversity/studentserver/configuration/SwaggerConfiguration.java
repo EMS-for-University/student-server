10f2b4cbfd29eaf028d082e30be8366f1fe2903e
Md. Shakil Hossain <shakilhossainpavel@gmail.com>
2023-04-06 05:30:09 +0600

    configuration updated

--- src/main/java/com/emsforuniversity/studentserver/configuration/SwaggerConfiguration.java
+++ src/main/java/com/emsforuniversity/studentserver/configuration/SwaggerConfiguration.java
@@ -16,7 +16,7 @@ public class SwaggerConfiguration {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.tanvirsojal.studentserver.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.emsforuniversity.studentserver.controller"))
                 .paths(regex("/api.*"))
                 .build();
     }
